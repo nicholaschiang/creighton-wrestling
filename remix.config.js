d4c461afba03e348c24da3e66a15a4d520dc9a33
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-05-28 18:33:16 -0700

    fix(remix.config.js): address websocket issues
    Fix for remix-run/remix#2958

--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,7 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  devServerPort: 8002,
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
 };
